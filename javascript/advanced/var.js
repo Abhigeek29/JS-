b8637a3bf8c0b6ce93889a6efccb6ff18ef9d9db
Abhigyan Sahoo <Sahooabhigyan24@gmail.com>
2025-08-13 20:15:03 +0530

    made changes in the array.js section

--- javascript/advanced/var.js
+++ javascript/advanced/var.js
@@ -23,3 +23,11 @@ function abc(){
 
 }
 abc();
+
+// main differences in var and {let,const}
+// Var is old es5 and other es6 so new
+// 2nd var is function scoped and other two blocked scope 
+// 3rd var attaches itself to window object while let and const dont 
+// ->> what is window object ( to see type window on browser console ( ctrl+shift+j))
+// Javascript mai kuch cheeze nhi hoti jo hum use karte hai , woh cheezo window object ke propertues hoti hai 
+
--- javascript/array.js
+++ javascript/array.js
@@ -10,5 +10,15 @@ a.shift(10); // used for deleting elements from the start of the array
 console.log(a);
 // Splice - value konse index se hatani hai aur kitni 
 
-a.splice(0,2); //Oth postion se hatani hai aur 2
+a.splice(0,2); //Oth postion se hatani hai aur 2 ---> in this case khali kardi hai
 console.log(a);
+
+
+// /// object hai ek individual ki details ko hold karna
+
+var a = {
+    Name:"Tom cruise",
+    age : 67,
+    height : 172,
+    jump : function(){}
+}
