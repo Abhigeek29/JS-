d887aa2a21f32cad5726985906e082217f9267a4
Abhigyan Sahoo <Sahooabhigyan24@gmail.com>
2025-08-13 19:29:05 +0530

    added array , loops , objects , functions in js

---
+++ javascript/array.js
@@ -0,0 +1,14 @@
+var a = [12,29]
+// push pop shift and unshift
+a.push(90);
+console.log(a);
+a.pop()
+console.log(a);
+a.unshift(10) // used for adding elements to the start of the array
+console.log(a);
+a.shift(10); // used for deleting elements from the start of the array
+console.log(a);
+// Splice - value konse index se hatani hai aur kitni 
+
+a.splice(0,2); //Oth postion se hatani hai aur 2
+console.log(a);
---
+++ javascript/loops.js
@@ -0,0 +1,9 @@
+// for loop - similar to that of c++
+
+// for in loop - just like python loop
+let a = [1,2,3,4,5,6]
+
+for(let x in a){
+    console.log(a[x]*2);
+}
+
